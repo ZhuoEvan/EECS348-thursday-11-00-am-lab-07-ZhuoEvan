61d62d20d5d5e41df385f274457e41abb2fa7803
ZhuoEvan <156927177+ZhuoEvan@users.noreply.github.com>
2025-03-23 22:08:36 -0500

    3/23/25 Update
    Worked on main() in temperature

--- EvanZhuo_Lab7_Temperature.c
+++ EvanZhuo_Lab7_Temperature.c
@@ -1,5 +1,12 @@
 //EvanZhuo_Lab7_Temperature.c
+#include <stdio.h>
 
+
+
+//Main Function | Created by Me
 int main() {
-    
+    int *temp; //Initialize a pointer to temp
+    printf("Enter the temperature: "); //Input Message for entering temperature
+    scanf("%d", temp);
+
 }
