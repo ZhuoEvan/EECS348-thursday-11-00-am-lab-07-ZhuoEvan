92f7372d09ed48b36719af192d3968569fd6dee5
ZhuoEvan <156927177+ZhuoEvan@users.noreply.github.com>
2025-03-24 15:29:47 -0500

    3/24/25 Update
    Worked on temperature.c

--- EvanZhuo_Lab7_Temperature.c
+++ EvanZhuo_Lab7_Temperature.c
@@ -21,10 +21,28 @@ float kelvin_to_celsius(float kelvin) {
     return kelvin - 273.15; //return the result of the conversion equation
 }
 
+int checkValid(int scale, int convert) {
+    if (scale == convert) {
+        return 0;
+    } else {
+        return 1;
+    }
+}
 //Main Function | Created by Me
 int main() {
     float *temp; //Initialize a pointer to temp
+    int *currScale; //Initialize a pointer to currScale
+    int *convertScale; //Initialize a pointer to convertScale
+
     printf("Enter the temperature: "); //Input Message for entering temperature
     scanf("%f", temp); //Input for temperature
-
+    printf("Choose the current scale (1) Celsius, (2) Fahrenheit, (3) Kelvin: "); //Input Message for entering current scale
+    scanf("%d", currScale); //Input for current scale
+    printf("Convert to (1) Celsius, (2) Fahrenheit, (3) Kelvin: "); //Input Message for entering convert scale
+    scanf("%d", convertScale); //Input for convert scale
+    if (checkValid(currScale, convertScale) == 1) {
+        print("valid");
+    } else {
+        printf("invalid");
+    }
 }
