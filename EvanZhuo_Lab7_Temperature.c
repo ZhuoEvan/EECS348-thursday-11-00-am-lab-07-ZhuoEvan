7a2ebc514548d7c4493e619a6c13b401ea5ac037
ZhuoEvan <156927177+ZhuoEvan@users.noreply.github.com>
2025-03-23 22:39:20 -0500

    3/23/25 Update
    Added conversion functions to temperature.c

--- EvanZhuo_Lab7_Temperature.c
+++ EvanZhuo_Lab7_Temperature.c
@@ -1,12 +1,30 @@
 //EvanZhuo_Lab7_Temperature.c
 #include <stdio.h>
 
+//Celsius to Fahrenheit Converter Function | Created by Me
+float celsius_to_fahrenheit(float celsius) {
+    return celsius * (9.0/5) + 32; //return the result of the conversion equation
+}
 
+//Fahrenheit to Celsius Converter Function | Created by Me
+float fahrenheit_to_celsius(float fahrenheit) {
+    return (fahrenheit - 32) * (5.0/9); //return the result of the conversion equation
+}
+
+//Celsius to Kelvin Converter Function | Created by Me
+float celsius_to_kelvin(float celsius) {
+    return celsius + 273.15; //return the result of the conversion equation
+}
+
+//Kelvin to Celsius Converter Function | Created by Me
+float kelvin_to_celsius(float kelvin) {
+    return kelvin - 273.15; //return the result of the conversion equation
+}
 
 //Main Function | Created by Me
 int main() {
-    int *temp; //Initialize a pointer to temp
+    float *temp; //Initialize a pointer to temp
     printf("Enter the temperature: "); //Input Message for entering temperature
-    scanf("%d", temp);
+    scanf("%f", temp); //Input for temperature
 
 }
